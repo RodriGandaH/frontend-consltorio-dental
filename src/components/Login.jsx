6918bb0d85f947d7c6179d38ca261694550c8a40
Rodrigo Gandarillas Heredia <rodrigo.gandarillas.heredia@gmail.com>
2022-11-13 10:02:27 -0400

    validacion de inputs  del login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import img_login from '../assets/images/login_img.jpg';
 import img_happy from '../assets/images/happy.png';
 
 const Login = () => {
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (user.trim().length === 0 || password.trim().length === 0) {
+      setError(true);
+    }
+  };
+
   return (
     <>
       <section className="login py-5 bg-light">
@@ -16,23 +27,38 @@ const Login = () => {
                 id="img_happy"
               />
               <h1 className="animate__animated animate__heartBeat">AliDent</h1>
-              <form action="">
+              <form onSubmit={handleSubmit}>
                 <div className="form-row py-5 pt-5">
                   <div className="offset-1 col-lg-10">
                     <input
+                      onChange={(e) => setUser(e.target.value)}
                       type="text"
                       placeholder="Usuario"
                       className="inp px-3"
                     />
+
+                    {error && user.length <= 0 ? (
+                      <label id="error">El campo Usuario es obligatorio</label>
+                    ) : (
+                      ''
+                    )}
                   </div>
                 </div>
                 <div className="form-row ">
                   <div className="offset-1 col-lg-10">
                     <input
+                      onChange={(e) => setPassword(e.target.value)}
                       type="password"
                       placeholder="Contraseña"
                       className="inp px-3"
                     />
+                    {error && password.length <= 0 ? (
+                      <label id="error">
+                        El campo Contraseña es obligatorio
+                      </label>
+                    ) : (
+                      ''
+                    )}
                   </div>
                 </div>
                 <div className="form-row py-5">
